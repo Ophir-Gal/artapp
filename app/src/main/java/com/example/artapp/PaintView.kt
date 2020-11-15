affb4116e7fea04ff9346437e9faaa7e27c310dc
cheymonty <45781686+cheymonty@users.noreply.github.com>
2020-11-15 17:12:29 -0500

    Fixed canvas download not working on older devices

--- app/src/main/java/com/example/artapp/PaintView.kt
+++ app/src/main/java/com/example/artapp/PaintView.kt
@@ -4,11 +4,15 @@ import android.content.ContentValues
 import android.content.Context
 import android.graphics.*
 import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
 import java.io.OutputStream
 
 
@@ -172,25 +176,43 @@ class PaintView : View {
     }
 
     fun downloadCanvas() {
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
-        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+        val bitmapToSave = Bitmap.createBitmap(canvasBitmap!!.width, canvasBitmap!!.height,
+                Bitmap.Config.ARGB_8888)
+        val tempCanvas = Canvas(bitmapToSave)
+        tempCanvas.drawColor(Color.WHITE) // set white background
+        tempCanvas.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint) // overlay the current drawing
 
-        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/ArtApp")
-        values.put(MediaStore.Images.Media.IS_PENDING, true)
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
 
-        val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/ArtApp")
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
 
-        if (uri != null) {
-            val bitmapToSave = Bitmap.createBitmap(canvasBitmap!!.width, canvasBitmap!!.height,
-                    Bitmap.Config.ARGB_8888)
-            val tempCanvas = Canvas(bitmapToSave)
-            tempCanvas.drawColor(Color.WHITE) // set white background
-            tempCanvas.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint) // overlay the current drawing
-            saveImageToStream(bitmapToSave, context.contentResolver.openOutputStream(uri))
-            values.put(MediaStore.Images.Media.IS_PENDING, false)
-            context.contentResolver.update(uri, values, null, null)
+            val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+            if (uri != null) {
+                saveImageToStream(bitmapToSave, context.contentResolver.openOutputStream(uri))
+                values.put(MediaStore.Images.Media.IS_PENDING, false)
+                context.contentResolver.update(uri, values, null, null)
+            }
+
+        } else {
+            val directory = File(Environment.getExternalStorageDirectory().toString() + separator + "ArtApp")
+
+            if (!directory.exists())
+                directory.mkdirs()
+
+            val fileName = System.currentTimeMillis().toString() + ".png"
+            val file = File(directory, fileName)
+            saveImageToStream(bitmapToSave, FileOutputStream(file))
+            if (file.absolutePath != null) {
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            }
         }
     }
 
