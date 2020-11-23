72505418b14cd878830cec01d82794aa5dfb687f
ophir <ophirgal2@gmail.com>
2020-11-22 22:59:56 -0500

    added welcome activity

---
+++ app/src/main/java/com/example/artapp/DrawingActivity.kt
@@ -0,0 +1,95 @@
+package com.example.artapp
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+class DrawingActivity : AppCompatActivity() {
+
+    private val mDBRef : DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+    lateinit var mCanvas: PaintView
+    lateinit var mBrushFab: FloatingActionButton
+    lateinit var mPaletteFab: FloatingActionButton
+    private var smallBrush = true
+    private var mColors = ArrayList<String>()
+    private var index = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_drawing)
+        mCanvas = findViewById(R.id.canvas)
+        mBrushFab = findViewById(R.id.mainBrushFab)
+        mPaletteFab = findViewById(R.id.colorFab)
+
+        mColors.add("black")
+        mColors.add("red")
+        mColors.add("blue")
+        mColors.add("cyan")
+        mColors.add("green")
+        mColors.add("yellow")
+
+
+        mBrushFab.setOnClickListener {
+            changeBrushSize()
+        }
+
+        mPaletteFab.backgroundTintList = ColorStateList.valueOf(Color.BLACK)
+        mPaletteFab.setOnClickListener{
+            changeBrushColor()
+        }
+    }
+
+    fun changeBrushSize() {
+        if (smallBrush) {
+            mCanvas.changeBrushSize(15f)
+            smallBrush = false
+        } else {
+            mCanvas.changeBrushSize(8f)
+            smallBrush = true
+        }
+
+    }
+
+    fun changeBrushColor() {
+        if (index < mColors.size - 1)
+            index++
+        else
+            index = 0
+
+        mCanvas.changeBrushColor(mColors[index])
+        mPaletteFab.backgroundTintList = ColorStateList.valueOf(Color.parseColor(mColors[index]))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menu.add(Menu.NONE, MENU_DOWNLOAD, Menu.NONE, "Download Canvas")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == MENU_DOWNLOAD) {
+            mCanvas.downloadCanvas()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+    companion object {
+        private const val MENU_DOWNLOAD = Menu.FIRST
+    }
+
+}
--- app/src/main/java/com/example/artapp/MainActivity.kt
+++ app/src/main/java/com/example/artapp/MainActivity.kt
@@ -1,95 +1,29 @@
 package com.example.artapp
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    private val mDBRef : DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
-    lateinit var mCanvas: PaintView
-    lateinit var mBrushFab: FloatingActionButton
-    lateinit var mPaletteFab: FloatingActionButton
-    private var smallBrush = true
-    private var mColors = ArrayList<String>()
-    private var index = 0
+    private lateinit var globalButton : Button
+    private lateinit var existingButton: Button
+    private lateinit var newButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
-        mCanvas = findViewById(R.id.canvas)
-        mBrushFab = findViewById(R.id.mainBrushFab)
-        mPaletteFab = findViewById(R.id.colorFab)
-
-        mColors.add("black")
-        mColors.add("red")
-        mColors.add("blue")
-        mColors.add("cyan")
-        mColors.add("green")
-        mColors.add("yellow")
-
-
-        mBrushFab.setOnClickListener {
-            changeBrushSize()
-        }
-
-        mPaletteFab.backgroundTintList = ColorStateList.valueOf(Color.BLACK)
-        mPaletteFab.setOnClickListener{
-            changeBrushColor()
-        }
+        globalButton = findViewById(R.id.global_bttn)
+        existingButton = findViewById(R.id.existing_bttn)
+        newButton = findViewById(R.id.new_bttn)
     }
 
-    fun changeBrushSize() {
-        if (smallBrush) {
-            mCanvas.changeBrushSize(15f)
-            smallBrush = false
-        } else {
-            mCanvas.changeBrushSize(8f)
-            smallBrush = true
-        }
-
+    fun onGlobalButtonClick(view: View) {
+        startActivity(Intent(this@MainActivity, DrawingActivity::class.java))
     }
-
-    fun changeBrushColor() {
-        if (index < mColors.size - 1)
-            index++
-        else
-            index = 0
-
-        mCanvas.changeBrushColor(mColors[index])
-        mPaletteFab.backgroundTintList = ColorStateList.valueOf(Color.parseColor(mColors[index]))
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        super.onCreateOptionsMenu(menu)
-
-        menu.add(Menu.NONE, MENU_DOWNLOAD, Menu.NONE, "Download Canvas")
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == MENU_DOWNLOAD) {
-            mCanvas.downloadCanvas()
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-
-
-    companion object {
-        private const val MENU_DOWNLOAD = Menu.FIRST
-    }
-
 }
